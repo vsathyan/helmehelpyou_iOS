4c1ced3545cb640dfdb84b1a7cac01fd0402ca18
Vineet Sathyan <vineet@meyouhealth.com>
2015-02-07 16:00:48 -0500

    initial project + cocoapod

---
+++ helpmehelpyou/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  helpmehelpyou
+//
+//  Created by Vineet Sathyan on 2/7/15.
+//  Copyright (c) 2015 MYH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ helpmehelpyou/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  helpmehelpyou
+//
+//  Created by Vineet Sathyan on 2/7/15.
+//  Copyright (c) 2015 MYH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
